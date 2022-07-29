2933478a7d60b117cf6b9fcbbf205abfde3ae406
wozller <brian.sanchez.wozller@gmail.com>
2022-07-29 12:42:49 -0400

    Create Java project
    The project will consist of two Java classes, Main and ContactsProgram.
    
    ContactsProgram will be our class for connecting to and interacting with
    the local PostgreSQL server on pgAdmin 4.
    
    Main will be our Driver class.

---
+++ JDBC_Demo/src/main/java/com/mycompany/jdbc_demo/ContactsProgram.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.jdbc_demo;
+
+/**
+ * This class will contain the methods used to connect to and interact with the
+ * local PostgreSQL server on pgAdmin 4.
+ * 
+ * @author wozller
+ */
+public class ContactsProgram {
+    
+} // End of ContactsProgram.
---
+++ JDBC_Demo/src/main/java/com/mycompany/jdbc_demo/Main.java
@@ -0,0 +1,15 @@
+package com.mycompany.jdbc_demo;
+
+/**
+ * Driver class.
+ * @author wozller
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+        
+    } // End of main method.
+    
+} // End of Main class.

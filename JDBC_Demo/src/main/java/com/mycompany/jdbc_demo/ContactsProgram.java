0c3f7fa629dbe59d17408c53177876ad9f745717
wozller <brian.sanchez.wozller@gmail.com>
2022-07-29 13:00:15 -0400

    Create a method to establish a connection with server
    Create a method in our ContactsProgram class to obtain access to the
    server. The method will establish said connection with a JDBC url, a
    username, and a password (user is prompted for the password). Using this
    information, a Connection object is generated by DriverManager. We can
    then presumably interact with said Connection object in order to
    communicate back and forth with the PostgreSQL server.

--- JDBC_Demo/src/main/java/com/mycompany/jdbc_demo/ContactsProgram.java
+++ JDBC_Demo/src/main/java/com/mycompany/jdbc_demo/ContactsProgram.java
@@ -1,5 +1,9 @@
-
 package com.mycompany.jdbc_demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
 
 /**
  * This class will contain the methods used to connect to and interact with the
@@ -8,5 +12,33 @@ package com.mycompany.jdbc_demo;
  * @author wozller
  */
 public class ContactsProgram {
+    
+    /**
+     * Establishes a connection with the local PostgreSQL server, return a type
+     * Connection object which we can then interact with elsewhere.
+     * @return 
+     */
+    public static Connection getServerConnection() {
+        
+        Connection connection = null;
+        
+        String url = "jdbc:postgresql://localhost:5432/jdbc_demo";
+        String username = "postgres";
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter pw for PostgreSQL server: ");
+        
+        String password = input.nextLine();
+        
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return connection;
+        
+    } // End of getServerConnection method.
     
 } // End of ContactsProgram.
